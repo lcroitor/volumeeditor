81e5fce09fab8ad14428c8db60f4c1d7f49c3e2b
Laura Croitoru <laura.croitoru@urz.uni-heidelberg.de>
2011-08-29 18:51:30 +0200

    functions changed

--- volumeeditor/rectangularmodel.py
+++ volumeeditor/rectangularmodel.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-from PyQt4.QtCore import pyqtSignal, QObject, QPointF, QRectF
+from PyQt4.QtCore import pyqtSignal, QObject, QRectF
                         
                          
                          
@@ -13,75 +13,55 @@ class RectangularModel(QObject):
     def __init__(self):
         QObject.__init__(self)
         self.pos = None
-        self.startPoint=QPointF()
-        self.lastPoint=QPointF()
-        self.currentPoint=QPointF()
+        self.startPoint=(-1,-1)
+        self.lastPoint=None
+        self.currentPoint=None
         #self.pen=QPen(Qt.black)
         #self.brush=QBrush(Qt.Dense6Pattern)
         self._dragMode = False
-        self._changeSelection=False
-        self.startPoint.x=-1
-        self.startPoint.y=-1
         self.rect = QRectF()
 
 
 
     def beginSelecting(self, pos):
-        self._dragMode = True
-        #self.scene.clear()
-        if self.startPoint.x == -1 and self.startPoint.y == -1:
-            self.startPoint.x, self.startPoint.y= pos[0], pos[1]
-            self._changeSelection=True
+        if self.startPoint == (-1,-1):
+            self.startPoint = (pos[0]+0.0001, pos[1]+0.0001)
+            self._dragMode = True
+            self.moveTo(pos)
+            
 
     
     def drawRectangle(self, p1, p2):
-        if p1.x < p2.x:
-            self.rect.x = p1.x
-            self.rect.width = p2.x-p1.x
+        if p1[0] < p2[0]:
+            self.rect.x = p1[0]
+            self.rect.width = p2[0]-p1[0]
         else:
-            self.rect.x = p2.x
-            self.rect.width = p1.x-p2.x
-        if p1.y < p2.y:
-            self.rect.y = p1.y
-            self.rect.height = p2.y-p1.y
+            self.rect.x = p2[0]
+            self.rect.width = p1[0]-p2[0]
+        if p1[1] < p2[1]:
+            self.rect.y = p1[1]
+            self.rect.height = p2[1]-p1[1]
         else:
-            self.rect.y = p2.y
-            self.rect.height = p1.y-p2.y
+            self.rect.y = p2[1]
+            self.rect.height = p1[1]-p2[1]
+        print 'rect.x =',  self.rect.x
+        print 'rect.y =', self.rect.y
+        print 'rect.width =', self.rect.width
+        print 'rect.height =', self.rect.height
         #self.scene.addRect(self.rect.x, self.rect.y, self.rect.width, self.rect.height, self.pen, self.brush )
         self.argsChanged.emit(self.rect.x, self.rect.y, self.rect.width, self.rect.height)
 
     def moveTo(self,pos):
-        if self._dragMode == True:
-            if self._changeSelection is True:
-                if pos[0]== self.lastPoint.x and pos[1] > self.startPoint.y and pos[1] < self.lastPoint.y:
-                    self.currentPoint.x = pos[0]
-                    self.currentPoint.y = self.lastPoint.y
-                    self.drawRectangle(self.startPoint, self.currentPoint)
-                    #self.selectedRectangle.emit(self.startPoint,self.currentPoint)
-                elif pos[0] == self.startPoint.x and pos[1] > self.startPoint.y and pos[1] < self.lastPoint.y:
-                    self.currentPoint.x = self.lastPoint.x
-                    self.currentPoint.y = self.lastPoint.y
-                    self.startPoint.x = pos[0]
-                    self.drawRectangle(self.startPoint, self.currentPoint)
-                    #self.selectedRectangle.emit(self.startPoint,self.currentPoint)
-                elif pos[0]== self.startPoint.y and pos[1] > self.startPoint.x and pos[1] < self.lastPoint.x:
-                    self.currentPoint.x = self.lastPoint.x
-                    self.currentPoint.y = self.lastPoint.y
-                    self.startPoint.y = pos[1]
-                    self.drawRectangle(self.startPoint, self.currentPoint)
-                    #self.selectedRectangle.emit(self.startPoint,self.currentPoint)
-                elif pos[0] == self.lastPoint.y and pos[1] > self.startPoint.x and pos[1] < self.lastPoint.x:
-                    self.currentPoint.x = self.lastPoint.x
-                    self.currentPoint.y = pos[1]
-                    self.drawRectangle(self.startPoint, self.currentPoint)
-                    #self.selectedRectangle.emit(self.startPoint,self.currentPoint)
+        if self._dragMode:
+            self.currentPoint= (pos[0], pos[1])
+            print 'current Point =', self.currentPoint[0], self.currentPoint[1]
+            print 'start point = ' , self.startPoint[0], self.startPoint[1]
+            self.drawRectangle(self.startPoint, self.currentPoint)
 
-            else:
-                self.currentPoint.x, self.currentPoint.y = pos[0], pos[1]
-                self.drawRectangle(self.startPoint, self.currentPoint)
-                #self.selectedRectangle.emit(self.startPoint,self.currentPoint)
         else:
-            self.currentPoint.x, self.currentPoint.y = pos[0], pos[1]
+            self.currentPoint= (pos[0], pos[1])
+            print 'current Point =' , self.currentPoint[0], self.currentPoint[1]
+            print 'start point = ' , self.startPoint[0], self.startPoint[1]
             self.drawRectangle(self.startPoint, self.currentPoint)
             #self.selectedRectangle.emit(self.startPoint,self.currentPoint)
             
@@ -91,8 +71,7 @@ class RectangularModel(QObject):
 
         
     def dumpSelecting(self, pos):
-        self.startPoint.x=-1
-        self.startPoint.y=-1
+        self.startPoint = (-1,-1)
         self._changeSelection=False
 
     
