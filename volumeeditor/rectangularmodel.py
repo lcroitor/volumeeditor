84732c72f31c2f47cc26157eecc7eacc2ee892d4
Laura Croitoru <laura.croitoru@urz.uni-heidelberg.de>
2011-09-06 12:48:44 +0200

    calculation of height and width of the rectangle

--- volumeeditor/rectangularmodel.py
+++ volumeeditor/rectangularmodel.py
@@ -8,28 +8,29 @@ from PyQt4.QtCore import pyqtSignal, QObject, QRectF
 class RectangularModel(QObject):
     newRectangle = pyqtSignal()
 
-    #argsChanged = pyqtSignal(int, int)
 
     def __init__(self):
         QObject.__init__(self)
+        self.sliceRect = None
         self.pos = None
         self.startPoint=(-1,-1)
-        self.lastPoint=None  
+        self.lastPoint=None
         self._selectedCorner = False
         self.rect = QRectF()
 
-    def beginSelecting(self, pos):
-        if self.startPoint == (-1,-1):
-            self.startPoint = (pos[0]+0.0001, pos[1]+0.0001)
-            self.moveTo(pos)
-        
-        else:
-            self.lastPoint = (self.rect.x + self.rect.height, self.rect.y + self.rect.width)
-            
 
+    def beginSelecting(self, pos, sliceRect):
+        self.sliceRect = sliceRect
+        if  self.startPoint == (-1,-1):
+            self.startPoint = (pos[0]+0.0001, pos[1]+0.0001)
+            self.startPointChanged.emit(self.startPoint[0],self.startPoint[1])
+            self.moveTo(pos)
+        else:
+            self.lastPoint = (self.rect.x + self.rect.width, self.rect.y + self.rect.height)
             ## select the bottom-right corner of the rectangle
             if self.lastPoint[0]-12 < pos[0] < self.lastPoint[0] + 12 and self.rect.y -12< pos[1] <self.rect.y + 12:
-                self.startPoint = (self.rect.x, self.rect.y + self.rect.width)
+                #self.startPoint = (self.rect.x, self.rect.y + self.rect.width)
+                self.startPoint = (self.rect.x, self.rect.y + self.rect.height)
                 self._selectedCorner = True
                 self.moveTo(pos)
         
@@ -52,26 +53,22 @@ class RectangularModel(QObject):
                 self._selectedCorner = True
                 self.moveTo(pos)
 
-
-
     def moveTo(self,pos):
         #draw rectangle
-        if self.startPoint[1] < pos[1]:
-            self.rect.y = self.startPoint[1]
-            self.rect.width = pos[1]-self.startPoint[1]
-        else:
-            self.rect.y = pos[1]
-            self.rect.width = self.startPoint[1]-pos[1]
         if self.startPoint[0] < pos[0]:
             self.rect.x = self.startPoint[0]
-            self.rect.height = pos[0]-self.startPoint[0]
+            self.rect.width = pos[0]-self.startPoint[0]
         else:
             self.rect.x = pos[0]
-            self.rect.height = self.startPoint[0]-pos[0]
-
-
+            self.rect.width = self.startPoint[0]-pos[0]
+        if self.startPoint[1] < pos[1]:
+            self.rect.y = self.startPoint[1]
+            self.rect.height = pos[1]-self.startPoint[1]
+        else:
+            self.rect.y = pos[1]
+            self.rect.height = self.startPoint[1]-pos[1]
+ 
         self.newRectangle.emit()
-
             
     def endSelecting(self, pos):
         if self._selectedCorner is True:
